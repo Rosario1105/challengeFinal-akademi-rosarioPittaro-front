60652220b7a77be9dac0a7a8dceb085280d6410f
Rosario Pittaro <rosario.pittaro1105@gmail.com>
2025-05-27 17:37:40 -0300

    Feat: Pantalla de inicio y pantalla de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-export default function App() {
+import { Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Routes>
+      <Route path="/" element={<h1 className="text-center mt-10 text-2xl">Bienvenido a Akademi</h1>} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
+
+export default App;
